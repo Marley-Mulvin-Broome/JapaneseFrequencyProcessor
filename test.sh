e8e7b1f3f6cb6822ac6d886a8bfccc1c78957aa6
Marley Mulvin Broome <marley.developer@gmail.com>
2023-08-24 20:08:22 +0900

    Updated test to not automatically make cov report, added an option to do so instead

--- test.sh
+++ test.sh
@@ -6,12 +6,15 @@ which pytest > /dev/null || { echo "Pytest not found, can't run tests."; exit 1;
 cov_report=html
 required_coverage=95
 open_report=false
+generate_report=false
+report=""
 
-while getopts "r:c:ho" opt; do
+while getopts "r:c:hog" opt; do
   case $opt in
     h)
-        echo "Usage: test.sh [-r <required coverage>] [-c <coverage report type>] [-o]"
-        echo "-o opens the coverage report in a browser after running tests"
+        echo "Usage: [-og] test.sh [-r <required coverage>] [-c <coverage report type>]"
+        echo "-o opens the coverage report in a browser after running tests. Requires -g"
+        echo "-g generates a coverage report"
         exit 0
         ;;
     r)
@@ -19,6 +22,9 @@ while getopts "r:c:ho" opt; do
       ;;
     c)
       cov_report=$OPTARG
+      ;;
+    g)
+      generate_report=true
       ;;
     o)
       open_report=true
@@ -29,9 +35,19 @@ while getopts "r:c:ho" opt; do
   esac
 done
 
+if [ "$open_report" = true ] && [ "$generate_report" = false ] ; then
+  echo "Can't open report without generating it. Use -g to generate a report."
+  exit 1
+fi
+
+if [ "$generate_report" = true ] ; then
+  report="--cov=src/jpfreq --cov-branch --cov-report=$cov_report --cov-fail-under=$required_coverage"
+fi
+
+
 # runs coverage with a html report
 # fails if cov < required_coverage
-pytest -n auto --cov=src/jpfreq --cov-branch --cov-report=$cov_report --cov-fail-under=$required_coverage tests/
+pytest -n auto $report tests/
 
 ruff src/jpfreq/*.py
 
