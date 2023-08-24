7db650a08e89905962fd13d1b8aaa5c5d9165b9b
Marley Mulvin Broome <marley.developer@gmail.com>
2023-08-24 19:52:20 +0900

    Added a requirements text file for github workflow

--- test.sh
+++ test.sh
@@ -5,11 +5,13 @@ which pytest > /dev/null || { echo "Pytest not found, can't run tests."; exit 1;
 # coverage report settings
 cov_report=html
 required_coverage=95
+open_report=false
 
-while getopts "r:c:h" opt; do
+while getopts "r:c:ho" opt; do
   case $opt in
     h)
-        echo "Usage: test.sh [-r <required coverage>] [-c <coverage report type>]"
+        echo "Usage: test.sh [-r <required coverage>] [-c <coverage report type>] [-o]"
+        echo "-o opens the coverage report in a browser after running tests"
         exit 0
         ;;
     r)
@@ -17,6 +19,9 @@ while getopts "r:c:h" opt; do
       ;;
     c)
       cov_report=$OPTARG
+      ;;
+    o)
+      open_report=true
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -27,3 +32,10 @@ done
 # runs coverage with a html report
 # fails if cov < required_coverage
 pytest -n auto --cov=src/jpfreq --cov-branch --cov-report=$cov_report --cov-fail-under=$required_coverage tests/
+
+ruff src/jpfreq/*.py
+
+
+if [ "$open_report" = true ] ; then
+  open htmlcov/index.html
+fi
