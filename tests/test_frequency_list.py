f3cd179a727a14e00177302d36980d46a599406f
Marley Mulvin Broome <marley.developer@gmail.com>
2023-08-24 18:47:53 +0900

    Fixed tests for frequency list

--- tests/test_frequency_list.py
+++ tests/test_frequency_list.py
@@ -60,11 +60,11 @@ test_data_text = [
             ),
         ),
         (
-            "これは日本語です",
+            "これは日本語です", # count '語' as a word
             TextInfo(
-                unique_words=2,
-                word_count=2,
-                unique_words_used_once=2,
+                unique_words=3,
+                word_count=3,
+                unique_words_used_once=3,
                 unique_words_used_once_percentage=100,
                 unique_kanji=3,
                 unique_kanji_used_once=3,
@@ -74,9 +74,9 @@ test_data_text = [
         (
             "これは日本語です。",
             TextInfo(
-                unique_words=2,
-                word_count=2,
-                unique_words_used_once=2,
+                unique_words=3,
+                word_count=3,
+                unique_words_used_once=3,
                 unique_words_used_once_percentage=100,
                 unique_kanji=3,
                 unique_kanji_used_once=3,
@@ -86,35 +86,35 @@ test_data_text = [
         (
             "これは日本語です。これは日本語です。",
             TextInfo(
-                unique_words=0,
-                word_count=4,
+                unique_words=3,
+                word_count=6,
                 unique_words_used_once=0,
                 unique_words_used_once_percentage=0,
-                unique_kanji=0,
+                unique_kanji=3,
                 unique_kanji_used_once=0,
                 unique_kanji_used_once_percentage=0,
             ),
         ),
         (
-            "この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。",
+            "この人は誰ですか",
             TextInfo(
-                unique_words=14,
-                word_count=14,
-                unique_words_used_once=14,
+                unique_words=3,
+                word_count=3,
+                unique_words_used_once=3,
                 unique_words_used_once_percentage=100,
-                unique_kanji=14,
-                unique_kanji_used_once=11,
-                unique_kanji_used_once_percentage=(11 / 14) * 100,
+                unique_kanji=2,
+                unique_kanji_used_once=2,
+                unique_kanji_used_once_percentage=100,
             ),
         ),
         (
             "本は本は本は本は本は本は本は本は本は本は本は本は",
             TextInfo(
-                unique_words=0,
+                unique_words=1,
                 word_count=12,
                 unique_words_used_once=0,
                 unique_words_used_once_percentage=0,
-                unique_kanji=0,
+                unique_kanji=1,
                 unique_kanji_used_once=0,
                 unique_kanji_used_once_percentage=0,
             ),
@@ -122,7 +122,7 @@ test_data_text = [
         (
             "これはこれはこれはこれはこれはこれはこれはこれはこれはこれはこれはこれは",
             TextInfo(
-                unique_words=0,
+                unique_words=1,
                 word_count=12,
                 unique_words_used_once=0,
                 unique_words_used_once_percentage=0,
@@ -146,9 +146,9 @@ test_data_text = [
         (
             "郵便番号",
             TextInfo(
-                unique_words=1,
-                word_count=1,
-                unique_words_used_once=1,
+                unique_words=2,
+                word_count=2,
+                unique_words_used_once=2,
                 unique_words_used_once_percentage=100,
                 unique_kanji=4,
                 unique_kanji_used_once=4,
@@ -158,9 +158,9 @@ test_data_text = [
         (
             "郵便番号は123-4567です",
             TextInfo(
-                unique_words=1,
-                word_count=1,
-                unique_words_used_once=1,
+                unique_words=2,
+                word_count=2,
+                unique_words_used_once=2,
                 unique_words_used_once_percentage=100,
                 unique_kanji=4,
                 unique_kanji_used_once=4,
@@ -194,9 +194,9 @@ test_data_text = [
         (
             "ググってください",
             TextInfo(
-                unique_words=1,
-                word_count=1,
-                unique_words_used_once=1,
+                unique_words=2,
+                word_count=2,
+                unique_words_used_once=2,
                 unique_words_used_once_percentage=100,
                 unique_kanji=0,
                 unique_kanji_used_once=0,
@@ -242,24 +242,24 @@ def test_text(text, expected_text_info, freq_list):
 
 test_data_texts = [
         (
-            ["日本語", "学校"],
+            ["日本", "学校"],
             TextInfo(
                 unique_words=2,
                 word_count=2,
                 unique_words_used_once=2,
                 unique_words_used_once_percentage=100,
-                unique_kanji=5,
-                unique_kanji_used_once=5,
+                unique_kanji=4,
+                unique_kanji_used_once=4,
                 unique_kanji_used_once_percentage=100,
             ),
         ),
         (
-            ["日本語", "学校", "日本語", "学校"],
+            ["日本", "学校", "日本", "学校"],
             TextInfo(
-                unique_words=0,
+                unique_words=2,
                 word_count=4,
                 unique_words_used_once=0,
-                unique_kanji=0,
+                unique_kanji=4,
                 unique_kanji_used_once=0,
                 unique_kanji_used_once_percentage=0,
             ),
@@ -291,7 +291,7 @@ test_data_texts = [
         (
             ["これは", "これは", "これは"],
             TextInfo(
-                unique_words=0,
+                unique_words=1,
                 word_count=3,
                 unique_words_used_once=0,
                 unique_words_used_once_percentage=0,
