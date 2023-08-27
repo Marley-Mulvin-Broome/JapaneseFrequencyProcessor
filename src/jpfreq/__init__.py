5d0ecb8f25126030a43fd09a9a9c2b4ae9690079
Marley Mulvin Broome <marley.developer@gmail.com>
2023-08-27 17:38:15 +0900

    Updated module to include readme, changed workflow for docs

--- src/jpfreq/__init__.py
+++ src/jpfreq/__init__.py
@@ -1,10 +1,3 @@
 """
-JPFreq is a Python package for calculating the frequency of words in Japanese text.
-
-The main uses for this package are:
-    - Creating Japanese frequency lists
-    - Creating Japanese vocabulary lists
-    - Exporting of them to various formats
-
-This package is built on top of Fugashi, which is a Cython wrapper for MeCab.
+.. include:: ../../README.md
 """
